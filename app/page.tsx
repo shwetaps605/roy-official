f678e144ca492d6983cdd8a68246c69afaa4e132
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-09-11 23:35:34 +0530

    first setup commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold"></h1>
+      </div>
+    </section>
+  );
+}
