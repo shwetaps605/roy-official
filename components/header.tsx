830e1f4de0c56bb1e0ab3f3f2a7c51e80a8fbd5e
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-09-14 23:29:40 +0530

    added some content to the header

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,20 @@
+import { Button } from "./ui/button"
 
 const Header = () => {
   return (
-    <div>header</div>
+    <div className="flex flex-row justify-between w-full px-5 py-7 bg-sky-950 text-white">
+        <div><h2>Shweta</h2></div>
+        <ul className="flex flex-row justify-between gap-5">
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Projects</li>
+            <li>Others</li>
+        </ul>
+        <div>
+            <Button>Resume</Button>
+        </div>
+    </div>
   )
 }
 
