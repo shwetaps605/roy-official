5df5ebf46512f51936af52931e84ecd07d0d5df9
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-09-16 23:29:04 +0530

    Worked on the hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header/>
-        <main>{children}</main>
-        <Footer/>
+        {children}
       </body>
     </html>
   );
